5f96d4fb4c4c86c09bc25b10aa298bb3b95d0b36
SakuraiCora <1479559098@qq.com>
2026-02-13 02:06:42 +0800

    ✨ feat: add on bot connect function

--- bot.py
+++ bot.py
@@ -3,6 +3,14 @@ from nonebot.adapters.onebot.v11 import Adapter as OneBotV11Adapter
 from nonebot.adapters.onebot.v11 import Bot
 
 from src.services.db import init_db
+from src.services.sync import (
+    load_blacklist_from_db,
+    load_groups_from_db,
+    load_members_from_db,
+    load_users_from_db,
+    sync_groups_from_api,
+    sync_users_from_api,
+)
 
 nonebot.init()
 
@@ -18,8 +26,12 @@ async def _on_startup() -> None:
 
 @driver.on_bot_connect
 async def _on_bot_connect(bot: Bot) -> None:
-    pass
-
+    await load_users_from_db()
+    await load_groups_from_db()
+    await load_members_from_db()
+    await load_blacklist_from_db()
+    await sync_users_from_api(bot)
+    await sync_groups_from_api(bot)
 
 nonebot.load_plugins("src/hooks/")
 nonebot.load_plugins("src/plugins/")
